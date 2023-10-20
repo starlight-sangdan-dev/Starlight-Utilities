fb575a79b369b37c2accfacc0da8d2f556f79dc3
StarlightSangdan <danju@starlight-sangdan.com>
2023-10-20 15:46:26 +0900

    feat: abstraction for core features

--- scss-generator/scss-generator.py
+++ scss-generator/scss-generator.py
@@ -21,12 +21,36 @@ SIGLE_PROPERTI_CLASS = {
 # 디자인 시스템에서 사용하는 간격이 4배수 값
 spacing_multiple = 4
 size_list = [x for x in range(1, 100) if x % spacing_multiple == 0]
-
-for size in size_list:
-    for class_name, propertiy in SIGLE_PROPERTI_CLASS.items():
-        with open("./dist/utilities.scss", "a+") as f:
-            f.write(f".{class_name}-{size}{{{propertiy}: {size}px;}}")
-    for class_name, properties in MULTIPLE_PROPERTIES_CLASS.items():
-        with open("./dist/utilities.scss", "a+") as f:
-            f.write(f".{class_name}-{size}{{{properties[0]}: {size}px; {properties[1]}: {size}px;}}")
     
+# Abstraction
+class SIGLE_PROPERTIY_UTILITY:
+    _data:dict = {}
+    
+    def __init__(self, data:dict):
+        self._data = data
+
+    def generateSCSS(self, px_size_list:list, file_name: str = "utility.scss"):
+        if (len(px_size_list) < 0):
+            raise Exception("px_size_list is empty")
+
+        for size in px_size_list:
+            for class_name, propertiy in self._data.items():
+                with open(f"./dist/{file_name}", "a+") as f:
+                    f.write(f".{class_name}-{size}{{{propertiy}: {size}px;}}")
+
+class DOUBLE_PROPERTIY_UTILITY:
+    _data:dict = {}
+    
+    def __init__(self, data:dict):
+        self._data = data
+
+    def generateSCSS(self, px_size_list:list, file_name: str = "utility.scss"):
+        if (len(px_size_list) < 0):
+            raise Exception("px_size_list is empty")
+
+        for size in px_size_list:
+            for class_name, properties in MULTIPLE_PROPERTIES_CLASS.items():
+                with open(f"./dist/{file_name}", "a+") as f:
+                    f.write(f".{class_name}-{size}{{{properties[0]}: {size}px; {properties[1]}: {size}px;}}")
+
+# TODO multiple property utility
